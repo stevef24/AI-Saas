61eab88fb498ad83af69585fd1a9c4ab59c55f21
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2024-02-29 20:10:12 +0000

    Add TransformationForm component and remove ThemeToggle component

--- lughai/src/app/(root)/transformation/add/[type]/page.tsx
+++ lughai/src/app/(root)/transformation/add/[type]/page.tsx
@@ -1,10 +1,14 @@
 import Header from "@/components/shared/Header";
 import React from "react";
 import { transformationTypes } from "@/constants";
+import TransformationForm from "@/components/shared/TransformationForm";
 const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
 	const transformation = transformationTypes[type];
 	return (
-		<Header title={transformation.title} subTitle={transformation.subTitle} />
+		<>
+			<Header title={transformation.title} subTitle={transformation.subTitle} />
+			<TransformationForm />
+		</>
 	);
 };
 
