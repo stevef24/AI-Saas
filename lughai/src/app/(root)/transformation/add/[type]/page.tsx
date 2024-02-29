8235f0fc0c730c9ab3d88443a0fd5984ce7f226e
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2024-02-29 21:16:54 +0000

    Add @radix-ui/react-select package and update TransformationForm component

--- lughai/src/app/(root)/transformation/add/[type]/page.tsx
+++ lughai/src/app/(root)/transformation/add/[type]/page.tsx
@@ -2,12 +2,28 @@ import Header from "@/components/shared/Header";
 import React from "react";
 import { transformationTypes } from "@/constants";
 import TransformationForm from "@/components/shared/TransformationForm";
-const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+import { auth } from "@clerk/nextjs";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+const AddTransformationTypePage = async ({
+	params: { type },
+}: SearchParamProps) => {
 	const transformation = transformationTypes[type];
+
+	const { userId } = auth();
+
+	if (!userId) redirect("/sign-in");
+
+	const user = await getUserById(userId);
 	return (
 		<>
 			<Header title={transformation.title} subTitle={transformation.subTitle} />
-			<TransformationForm />
+			<TransformationForm
+				action="Add"
+				userId={user._id}
+				type={transformation.type as TransformationTypeKey}
+				creditBalance={0}
+			/>
 		</>
 	);
 };
--- lughai/src/components/shared/TransformationForm.tsx
+++ lughai/src/components/shared/TransformationForm.tsx
@@ -13,26 +13,53 @@ import {
 	FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { defaultValues } from "@/constants";
+import { CustomField } from "./CustomField";
 
-const formSchema = z.object({
-	username: z.string().min(2).max(20),
+export const formSchema = z.object({
+	title: z.string(),
+	aspectRatio: z.string().optional(),
+	color: z.string().optional(),
+	prompt: z.string().optional(),
+	publicId: z.string(),
 });
 
-const TransformationForm = () => {
+const TransformationForm = ({
+	action,
+	data = null,
+}: TransformationFormProps) => {
+	const initialValues =
+		data && action === "Update"
+			? {
+					title: data.title,
+					aspectRatio: data.aspectRatio,
+					color: data.color,
+					prompt: data.prompt,
+					publicId: data.publicId,
+			  }
+			: defaultValues;
+
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
-		defaultValues: {
-			username: "",
-		},
+		defaultValues: initialValues,
 	});
 
-	// 2. Define a submit handler.
 	function onSubmit(values: z.infer<typeof formSchema>) {
-		// Do something with the form values.
-		// ✅ This will be type-safe and validated.
 		console.log(values);
 	}
-	return <div>TransformationForm</div>;
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+				<CustomField
+					control={form.control}
+					name="title"
+					formLabel="Image Title"
+					className="w-full"
+					render={({ field }) => <Input {...field} className="input-field" />}
+				/>
+			</form>
+		</Form>
+	);
 };
 
 export default TransformationForm;
