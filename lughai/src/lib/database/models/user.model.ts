72e143dc4435a203aab0a38669ccdda28c645373
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2024-02-29 17:23:34 +0000

    Refactor user model fields***

--- lughai/src/lib/database/models/user.model.ts
+++ lughai/src/lib/database/models/user.model.ts
@@ -3,14 +3,39 @@ import { Schema, model, models } from "mongoose";
 import { Document } from "mongoose";
 
 const UserSchema = new Schema({
-	clerkId: { type: String, required: true, unique: true },
-	firstName: { type: String, required: true },
-	lastName: { type: String, required: true },
-	email: { type: String, required: true, unique: true },
-	username: { type: String, required: true, unique: true },
-	creditBalance: { type: Number, default: 10 },
-	planId: { type: Number, default: 1 },
-	photo: { type: URL },
+	clerkId: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	photo: {
+		type: String,
+		required: true,
+	},
+	firstName: {
+		type: String,
+	},
+	lastName: {
+		type: String,
+	},
+	planId: {
+		type: Number,
+		default: 1,
+	},
+	creditBalance: {
+		type: Number,
+		default: 10,
+	},
 });
 
 export interface IUser extends Document {
